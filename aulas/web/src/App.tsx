7bea1a234f3da50aeac15db19fe66991824a38ce
rickydomingos <ricardodomingos97@gmail.com>
2022-05-03 17:26:22 -0300

    feedback span added day1

--- aulas/web/src/App.tsx
+++ aulas/web/src/App.tsx
@@ -1,23 +1,5 @@
-interface ButtonProps {
-  // text?: string
-  text: string;
+import { Widget } from "./components/Widget";
+
+export function App() {
+  return <Widget/>
 }
-
-
-function Button(props: ButtonProps) {
-  // return <button>{props.text}</button> (ou pode ser:)
-  return <button className="bg-[#8257ec] text-violet-100 p-2 h-10 rounded hover:bg-red-700 transition-colors">{props.text ?? 'Default'}</button>
-}
-
-function App() {
-  return (
-    <div className="flex gap-2">
-      <Button text="enviar"/>
-      <Button text="oi pessoas"/>
-      <Button />
-    </div>
-  )
-
-}
-
-export default App
---
+++ aulas/web/src/components/Widget.tsx
@@ -0,0 +1,16 @@
+import { ChatTeardropDots } from "phosphor-react"
+
+export function Widget() {
+    return (
+        <div className="absolute bottom-4 right-5">
+            <button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center">
+            <ChatTeardropDots className="h-6 w-6" />
+
+            <span className="max-w-0 overflow-hidden">
+                <span className="pl-2"></span>
+                Feedback
+            </span>
+        </button>
+        </div>
+    )
+}
