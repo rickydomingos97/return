96e5658630beaab9b3049f57be2d31fb9cc5ce01
rickydomingos <ricardodomingos97@gmail.com>
2022-05-03 17:32:38 -0300

    widget added transition

--- aulas/web/src/components/Widget.tsx
+++ aulas/web/src/components/Widget.tsx
@@ -3,10 +3,10 @@ import { ChatTeardropDots } from "phosphor-react"
 export function Widget() {
     return (
         <div className="absolute bottom-4 right-5">
-            <button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center">
+            <button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
             <ChatTeardropDots className="h-6 w-6" />
 
-            <span className="max-w-0 overflow-hidden">
+            <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 easy-linear">
                 <span className="pl-2"></span>
                 Feedback
             </span>
