e26ce0a0ed293c65db010820f77cd3cf36aabebf
rickydomingos <ricardodomingos97@gmail.com>
2022-05-03 18:51:03 -0300

    close open toogle id done

--- aulas/web/src/components/Widget.tsx
+++ aulas/web/src/components/Widget.tsx
@@ -1,9 +1,26 @@
 import { ChatTeardropDots } from "phosphor-react"
+import { useState } from "react"
+/**
+ * Estado eh uma variavel do JS como qualquer outra, mas que fica watching as alteracoes
+ * no codigo para renderizar o componente com as alteracoes do estado
+ * ** const [] = useState() tem () pois eh uma function, e comeca com false booleano
+ * 
+ * { isWidgetOpen ? <p>Hello world</p> : null} pode ser substituido por { isWidgetOpen && <p>Hello world</p>}
+ * pois nao temos uma condicao ternaria aqui
+ */
 
 export function Widget() {
+    const [isWidgetOpen, setWidgetOpen] = useState(false)
+
+    function toogleWidgetVisibility() {
+        setWidgetOpen(!isWidgetOpen)
+    }
+
     return (
         <div className="absolute bottom-4 right-5">
-            <button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+            { isWidgetOpen && <p>Hello world</p>}
+            
+            <button onClick={toogleWidgetVisibility} className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
             <ChatTeardropDots className="h-6 w-6" />
 
             <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 easy-linear">
