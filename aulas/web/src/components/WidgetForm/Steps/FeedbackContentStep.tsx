085ff86738bb02a5ecdc29e493533117d8274342
rickydomingos <ricardodomingos97@gmail.com>
2022-05-04 11:40:40 -0300

    back button added

--- aulas/web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ aulas/web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,28 +1,42 @@
+import { ArrowLeft } from "phosphor-react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButtom } from "../../CloseButtom";
 
-interface FeesbackContentStepProps {
-    feedbackType: FeedbackType;
+interface FeedbackContentStepProps {
+  feedbackType: FeedbackType;
+  onFeedbackRestartRequested: () => void;
 }
 
-export function FeedbackContentStep({ feedbackType}: FeesbackContentStepProps) {
-    const feedbackTypeInfo = feedbackTypes[feedbackType]
+export function FeedbackContentStep({
+  feedbackType,
+  onFeedbackRestartRequested,
+}: FeedbackContentStepProps) {
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
 
+  return (
+    //fragment no react <> para poder envolver os conteudos
+    <>
+      <header>
+        <button
+          type="button"
+          className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+          onClick={onFeedbackRestartRequested}
+        >
+          <ArrowLeft weight="bold" className="w-4 h-4" />
+        </button>
 
-        return (
-            //fragment no react <> para poder envolver os conteudos
-          <>
-            <header>
-              <span className="text-xl leading-6">
-                  <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6" />
-                  {feedbackTypeInfo.title}
-              </span>
-              <CloseButtom />
-            </header>
-      
-            <div className="flex py-8 gap-2 w-full">
-              
-            </div>
-          </>
-        )
+        <span className="text-xl leading-6 flex items-center gap-2">
+          <img
+            src={feedbackTypeInfo.image.source}
+            alt={feedbackTypeInfo.image.alt}
+            className="w-6 h-6"
+          />
+          {feedbackTypeInfo.title}
+        </span>
+        <CloseButtom />
+      </header>
+
+      <div className="flex py-8 gap-2 w-full"></div>
+    </>
+  );
 }
--- aulas/web/src/components/WidgetForm/index.tsx
+++ aulas/web/src/components/WidgetForm/index.tsx
@@ -39,6 +39,10 @@ export function WidgetForm() {
 
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
 
+    function handleRestartFeedback() {
+      setFeedbackType(null)
+    }
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
 
@@ -46,16 +50,16 @@ export function WidgetForm() {
       {!feedbackType ? ( //isso vai ser exportado para feedbacktypestep
         <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
       ) : (
-        <FeedbackContentStep feedbackType={feedbackType}/>
+        <FeedbackContentStep feedbackType={feedbackType}
+        onFeedbackRestartRequested={handleRestartFeedback}
+        />
       )}
 
 
 
       <footer className="text-xs text-neutral-400">
         Feito com ♥ pela{" "}
-        <a
-          className="underline underline-offset-2"
-          href="https://rocketseat.com.br"
+        <a className="underline underline-offset-2" href="https://rocketseat.com.br"
         >
           Rocketseat
         </a>
