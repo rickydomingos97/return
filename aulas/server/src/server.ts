e13d7c93fc8caa083eede17551c69d40fe94574f
rickydomingos <ricardodomingos97@gmail.com>
2022-05-06 14:36:34 -0300

    ending email is ok

--- aulas/server/src/server.ts
+++ aulas/server/src/server.ts
@@ -1,9 +1,20 @@
 import express from 'express'
+import nodemailer from 'nodemailer'
 import { prisma } from './prisma';
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "c64f1c6522ae35",
+      pass: "663cc6b19724c7"
+    }
+  });
+
 
 app.post('/feedbacks', async (req, res) => {
     const { type, comment, screenshot } = req.body;
@@ -14,6 +25,18 @@ app.post('/feedbacks', async (req, res) => {
             comment: comment,
             screenshot: screenshot,
         }
+    })
+
+    await transport.sendMail({
+        from: 'Equipe Feedget <oi@feedget.com>',
+        to: 'Ricardo Domingos <ricardodomingos97@gmail.com>',
+        subject: 'Novo feedback',
+        html: [
+            `<div style="font-family: sans-serif; font-family: 16px; color: #111; ">`,
+            `<p>Tipo do feedback ${type}</p>`,
+            `<p>Comentario ${comment}</p>`,
+            `</div>`
+        ].join('\n')
     })
 
     return res.status(201).json({ data: feedback });
