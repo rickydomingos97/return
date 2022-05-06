c676afc97c9f75bbca964a24f6b34960db7206bd
rickydomingos <ricardodomingos97@gmail.com>
2022-05-06 14:04:47 -0300

    prisma updated ok

--- aulas/server/src/server.ts
+++ aulas/server/src/server.ts
@@ -1,17 +1,33 @@
 import express from 'express'
+import { prisma } from './prisma';
 
 const app = express();
 
- app.get('/user', (req, res) => {
-    return res.send('oi pessoas de bem')
+app.use(express.json());
+
+app.post('/feedbacks', async (req, res) => {
+    const { type, comment, screenshot } = req.body;
+
+    const feedback = await prisma.feedback.create({
+        data: {
+            type: type,
+            comment: comment,
+            screenshot: screenshot,
+        }
+    })
+
+    return res.status(201).json({ data: feedback });
 })
 
-app.get('/order', (req, res) => {
-    return res.send('casa de papel')
-})
-
-
-
-app.listen(3333, ()=> {
+app.listen(3333, () => {
     console.log('HTTP o servidor esta rodando')
-})
+});
+
+
+
+// GET - buscar infos
+// POST - cadastrar informacoes
+// PUT - atualizar infos de uma entidade
+// PATCH - atualizar uma unica info de uma entidade
+// DELETE - deletar uma info
+// para o server entender que estamos mandando um jsonk
