8a8f15150d785ecfa5f2e6e5cf122fb3b95c2666
rickydomingos <ricardodomingos97@gmail.com>
2022-05-12 01:05:08 -0300

    day 4 - mobile expo installed

--- aulas/web/src/components/WidgetForm/index.tsx
+++ aulas/web/src/components/WidgetForm/index.tsx
@@ -41,13 +41,14 @@ export function WidgetForm() {
   const [feedbackSent, setFeedbackSent] = useState(false);
 
   function handleRestartFeedback() {
+    setFeedbackSent(false)
     setFeedbackType(null);
   }
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       {feedbackSent ? (
-        <FeedbackSuccessStep />
+        <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback}/>
       ) : (
         <>
           {!feedbackType ? ( //isso vai ser exportado para feedbacktypestep
