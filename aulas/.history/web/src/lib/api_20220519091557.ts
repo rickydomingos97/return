0f80ea5857274ec594fe462dbb06c926f6ceb911
rickydomingos <ricardodomingos97@gmail.com>
2022-05-19 09:25:30 -0300

    vite api url added and .env connected

--- aulas/.history/web/src/lib/api_20220519091557.ts
+++ aulas/.history/web/src/lib/api_20220519091557.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const api = axios.create({
    // baseURL: 'http://localhost:3333/' before
-   baseURL: import.meta.env.VITE_API_URL, // restart server after changes .env
+   baseURL: import.meta.env.BASE_URL,
 })
